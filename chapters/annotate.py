774a3ff3fd684c6bb2cb6933add819ae5f865995
abhisaini <abhisni@iitk.ac.in>
2018-11-12 18:15:36 +0530

    chap 10,11,12,13,14,16,18

--- chapters/annotate.py
+++ chapters/annotate.py
@@ -1,27 +1,26 @@
-import json 
-import numpy as np 
+import json
+import numpy as np
 
 for i in range(8):
 
     ch = raw_input("Enter chapter Number between 10 and 18 [both inclusive]\n")
     with open('chapter'+str(ch)+'.json','r') as f:
-    	data = json.load(f)
+        data = json.load(f)
 
-    	verses = len(data)
-    	random = []
+        verses = len(data)
+        random = []
 
-    	count = 0
-    	while(count<7 and count<verses):
+        count = 0
+        while(count<7 and count<verses):
 
-    		x = np.random.randint(0,verses)
+            x = np.random.randint(0,verses)
 
-    		if x in random:
-    			continue
-    		else:
-    			random.append(x)
+            if x in random:
+                continue
+            else:
+                random.append(x)
 
-    			ann = raw_input("\nThe Shloka is\n"+data[x]['meaning'].encode('UTF8')+'\n')
-
+                ann = raw_input("\nThe Shloka is\n"+data[x]['meaning'].encode('UTF8')+'\n')
                 ann = ann.split(',')
 
                 list_v = []
@@ -32,14 +31,12 @@ for i in range(8):
 
                 with open('verses.json','r') as file:
                     list_d = json.load(file)
-
                 index = -1
-
                 for shloka in list_d:
                     index += 1
                     if(shloka['meaning'].encode('UTF8') == data[x]['meaning'].encode('UTF8')):
                         list_d[index]['vector'] = list_v
-            
+
                 with open('verses.json','w') as file:
                     file.write(json.dumps(list_d))
 
